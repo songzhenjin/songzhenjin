85717702ea4c97bcf158042d40770908a15afa00
‘songzhenjin’ <915597930@qq.com>
2022-09-02 21:07:09 +0800

    Site updated: 2022-09-02 21:07:08

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "FF9900",
+    "citySize": "16",
+    "aqiColor": "4A86E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "191",
+    "top": "19",
+    "font-family": "Ayuan",
+    "key": "5193df47d9ce44f9be30d2896c4fef89"
+  }
+}
